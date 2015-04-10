271c6d7e2f3ec44bbf4300d7c6f99cd8e85cd856
denis <denis@Hoteia-Laptop>
2015-04-10 17:17:22 +0200

    more

---
+++ src/main/java/org/hoteia/jqgrid/mapping/JqGridFilterToolbarPojo.java
@@ -0,0 +1,106 @@
+package org.hoteia.jqgrid.mapping;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class JqGridFilterToolbarPojo implements Serializable {
+
+    private String value;
+
+    @JsonProperty("autosearch")
+    private boolean autoSearch;
+
+    // private String beforeSearch;
+    // private String afterSearch;
+    // private String beforeClear;
+    // private String afterClear;
+
+    @JsonProperty("searchOnEnter")
+    private boolean searchOnEnter;
+
+    @JsonProperty("stringResult")
+    private boolean stringResult;
+
+    @JsonProperty("groupOp")
+    private String groupOp;
+
+    @JsonProperty("defaultSearch")
+    private String defaultSearch;
+
+    @JsonProperty("searchOperators")
+    private boolean searchOperators;
+
+    @JsonProperty("operandTitle")
+    private String operandTitle;
+
+    // private String operands;
+    // private String odata;
+    
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean isAutoSearch() {
+        return autoSearch;
+    }
+
+    public void setAutoSearch(boolean autoSearch) {
+        this.autoSearch = autoSearch;
+    }
+
+    public boolean isSearchOnEnter() {
+        return searchOnEnter;
+    }
+
+    public void setSearchOnEnter(boolean searchOnEnter) {
+        this.searchOnEnter = searchOnEnter;
+    }
+
+    public boolean isStringResult() {
+        return stringResult;
+    }
+
+    public void setStringResult(boolean stringResult) {
+        this.stringResult = stringResult;
+    }
+
+    public String getGroupOp() {
+        return groupOp;
+    }
+
+    public void setGroupOp(String groupOp) {
+        this.groupOp = groupOp;
+    }
+
+    public String getDefaultSearch() {
+        return defaultSearch;
+    }
+
+    public void setDefaultSearch(String defaultSearch) {
+        this.defaultSearch = defaultSearch;
+    }
+
+    public boolean isSearchOperators() {
+        return searchOperators;
+    }
+
+    public void setSearchOperators(boolean searchOperators) {
+        this.searchOperators = searchOperators;
+    }
+
+    public String getOperandTitle() {
+        return operandTitle;
+    }
+
+    public void setOperandTitle(String operandTitle) {
+        this.operandTitle = operandTitle;
+    }
+
+}
--- src/main/java/org/hoteia/jqgrid/mapping/JqGridSearchOptionPojo.java
+++ src/main/java/org/hoteia/jqgrid/mapping/JqGridSearchOptionPojo.java
@@ -3,17 +3,91 @@ package org.hoteia.jqgrid.mapping;
 import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class JqGridSearchOptionPojo implements Serializable {
 
-	private String test;
+    @JsonProperty("dataUrl")
+	private String dataUrl;
+    
+//    private String buildSelect;
+//    private String dataInit;
+    
+    @JsonProperty("dataEvents")
+    private String[] dataEvents;
+    
+//    private String attr;
+    
+    @JsonProperty("searchhidden")
+    private boolean searchHidden;
+    
+    @JsonProperty("sopt")
+    private String[] sOpt;
+    
+    @JsonProperty("defaultValue")
+    private String defaultValue;
+    
+    @JsonProperty("value")
+    private String value;
+    
+    @JsonProperty("clearSearch")
+    private boolean clearSearch;
+
+    public String getDataUrl() {
+        return dataUrl;
+    }
+
+    public void setDataUrl(String dataUrl) {
+        this.dataUrl = dataUrl;
+    }
+
+    public String[] getDataEvents() {
+        return dataEvents;
+    }
+
+    public void setDataEvents(String[] dataEvents) {
+        this.dataEvents = dataEvents;
+    }
+
+    public boolean isSearchHidden() {
+        return searchHidden;
+    }
+
+    public void setSearchHidden(boolean searchHidden) {
+        this.searchHidden = searchHidden;
+    }
+
+    public String[] getsOpt() {
+        return sOpt;
+    }
+
+    public void setsOpt(String[] sOpt) {
+        this.sOpt = sOpt;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean isClearSearch() {
+        return clearSearch;
+    }
+
+    public void setClearSearch(boolean clearSearch) {
+        this.clearSearch = clearSearch;
+    }
 	
-	public String getTest() {
-		return test;
-	}
-	
-	public void setTest(String test) {
-		this.test = test;
-	}
 }
