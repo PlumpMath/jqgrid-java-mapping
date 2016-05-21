db3e1db8bbc2aaa292584d309655ed297dd6e844
Denis <Denis@Hoteia-M4800>
2016-05-21 13:22:00 +0200

    tools

---
+++ src/main/java/org/hoteia/jqgrid/util/JQGridTool.java
@@ -0,0 +1,22 @@
+package fr.hoteia.opticmarketplace.web.mvc.jqgrid;
+
+import org.hoteia.jqgrid.mapping.JqGridSearchOptionsPojo;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JQGridTool {
+
+	public static ObjectMapper getObjectMapper() {
+		 ObjectMapper mapper = new ObjectMapper();
+		 mapper.setSerializationInclusion(Include.NON_NULL);
+		 return mapper;
+	}
+	
+    public static JqGridSearchOptionsPojo buildDefaultJqGridSearchOptions(){
+        JqGridSearchOptionsPojo searchOptionsPojo = new JqGridSearchOptionsPojo();
+        searchOptionsPojo.setClearSearch(true);
+        searchOptionsPojo.setDefaultValue("");
+        return searchOptionsPojo;
+    }
+}
